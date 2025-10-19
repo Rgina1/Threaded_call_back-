b771f89c5bea8240660e7d57e9f085884cc4b923
Rgina1 <133843105+Rgina1@users.noreply.github.com>
2025-10-19 12:57:32 -0400

    cleanup established

--- walk_Test.py
+++ walk_Test.py
@@ -1,21 +1,25 @@
 from Shifter import Shifter
+import RPi.GPIO as GPIO
 import random
 walkValues = [x for x in range(1,(2**8)+1) if ((x & x-1)==0)] 
 Shfiter_object = Shifter(23,24,25)
 i = random.randint(0,len(walkValues)-1)
-while True:
-    increment = random.randint(0,1) # zero is backwards -1
-    if (increment == 0):
-        increment = -1
+try:
+    while True:
+        increment = random.randint(0,1) # zero is backwards -1
+        if (increment == 0):
+            increment = -1
 
-    if(i > 0 and i < len(walkValues)-1):
-        i=i+increment
-    elif (i==0):
-        i=i+1
-    elif(i==len(walkValues)-1):
-        i=i-1
-    pattern = walkValues[i]
+        if(i > 0 and i < len(walkValues)-1):
+            i=i+increment
+        elif (i==0):
+            i=i+1
+        elif(i==len(walkValues)-1):
+            i=i-1
+        pattern = walkValues[i]
 
-    Shfiter_object.shiftByte(pattern)
+        Shfiter_object.shiftByte(pattern)
+except KeyboardInterrupt:
+    GPIO.cleanup()
 
 
